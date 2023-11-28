9d5b7da738dccb94bfcf21a25f419e8f83f56a8d
mortezashoghi <mortaza.shoghi@gmail.com>
2023-11-28 11:55:50 +0330

    add api call https,fetch,axios

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {fulinfo,tavan,iseven,adduser,userlist} from "../modules/user.js"
+import {fulinfo,tavan,iseven,adduser,userlist,getAll,getAllf,getAlla} from "../modules/user.js"
 import {pool} from "../modules/db.js"
 import crypto from "crypto";
 import { promises } from "dns";
@@ -20,9 +20,13 @@ router.get('/tavan', tavan);
 router.get('/even/:age',iseven);
 router.get('/userlist',userlist);
 router.get('/adduser', adduser);
+<<<<<<< HEAD
 router.post("/register", adduser);
 router.get('/login', (req, res) => {
     res.render('index', {signin:"true"})
 });
+=======
+router.get('/getall',getAll);
+>>>>>>> 8edac3c (all type of call api (https,axios,frtch) in user.js)
 
 export default router;
